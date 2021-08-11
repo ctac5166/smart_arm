8a087952de6de7049f0761b36195c70f202eada2
ctac5166 <ctac23062006@gmail.com>
2021-08-11 17:26:02 +0500

    last-update
    last 11/08/2021

--- VR/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ VR/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -113,6 +113,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean buttonY
+        {
+            get
+            {
+                return SteamVR_Actions.default_buttonY;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
--- VR/Assets/vr_server.cs
+++ VR/Assets/vr_server.cs
@@ -8,6 +8,7 @@ using Valve.VR;
 
 public class vr_server : MonoBehaviour
 {
+    public Stea bool_y;
     public SteamVR_Action_Vector2 vector;
     public MeshRenderer mesh;
     public Vector3 coords_delay;
@@ -17,6 +18,7 @@ public class vr_server : MonoBehaviour
     public GameObject[] parts;
     Vector3 start_pos;
     public float opened = 0;
+    public float opened2 = 0;
     private void Start()
     {
         start_pos = transform.position;
@@ -28,6 +30,11 @@ public class vr_server : MonoBehaviour
         if(vector.axis.x * Time.deltaTime != 0) Debug.Log(vector.axis.x * Time.deltaTime);
         if (opened > 1) opened = 1;
         if (opened < 0) opened = 0;
+
+        opened2 += vector.axis.y * Time.deltaTime;
+        if (vector.axis.y * Time.deltaTime != 0) Debug.Log(vector.axis.y * Time.deltaTime);
+        if (opened2 > 1) opened2 = 1;
+        if (opened2 < 0) opened2 = 0;
     }
     IEnumerator PostRequest(string url)
     {
@@ -35,13 +42,13 @@ public class vr_server : MonoBehaviour
         {
             yield return new WaitForSeconds(0.01f);
             MyClass myObject = new MyClass();
-            myObject.xyz.Add(((gameObject.transform.position.z) - start_pos.z) * multiplayer + coords_delay.x);
+            myObject.xyz.Add(((gameObject.transform.position.z * -1) + start_pos.z) * multiplayer + coords_delay.x);
             myObject.xyz.Add((gameObject.transform.position.x - start_pos.x) * multiplayer + coords_delay.z);
             myObject.xyz.Add((gameObject.transform.position.y - start_pos.y) * multiplayer + coords_delay.y);
             myObject.speed = speed;
             myObject.opened = opened;
             myObject.rotation = transform.eulerAngles.y;
-            myObject.rotation2 = transform.eulerAngles.z - 180;
+            myObject.rotation2 = opened2 * 360;
             string json = JsonUtility.ToJson(myObject);
             //Debug.Log(json);
 
@@ -84,12 +91,12 @@ public class vr_server : MonoBehaviour
                     return_text[1] = return_text[1].Replace(".", sp);
                     float float1 = float.Parse(return_text[1]);
                     parts[4].transform.eulerAngles = new Vector3(
+                        0,
                         -float1 * Mathf.Rad2Deg,
-                        90,
-                        90
+                        0
                     );
 
-                    /*sp = NumberFormatInfo.CurrentInfo.CurrencyDecimalSeparator;
+                    sp = NumberFormatInfo.CurrentInfo.CurrencyDecimalSeparator;
                     return_text[2] = return_text[2].Replace(".", sp);
                     float1 = float.Parse(return_text[2]);
                     parts[0].transform.eulerAngles = new Vector3(
@@ -100,21 +107,24 @@ public class vr_server : MonoBehaviour
 
                     sp = NumberFormatInfo.CurrentInfo.CurrencyDecimalSeparator;
                     return_text[3] = return_text[3].Replace(".", sp);
-                    float1 = float.Parse(return_text[1]);
-                    parts[2].transform.eulerAngles = new Vector3(
-                        0,
-                        0,
-                        float1 * Mathf.Rad2Deg
-                    );
+                    float1 = float.Parse(return_text[3]);
 
-                    return_text[4] = return_text[4].Replace(".", sp);
-                    float1 = float.Parse(return_text[4]);
-                    parts[3].transform.eulerAngles = new Vector3(
+                    Debug.Log(-float1 * Mathf.Rad2Deg);
+
+                    parts[2].transform.eulerAngles = new Vector3(
                         -float1 * Mathf.Rad2Deg,
                         0,
                         0
                     );
 
+                    return_text[4] = return_text[4].Replace(".", sp);
+                    float1 = float.Parse(return_text[4]);
+                    parts[3].transform.eulerAngles = new Vector3(
+                        myObject.rotation2,
+                        0,
+                        0
+                    );
+                    /*
                     return_text[3] = return_text[3].Replace(".", sp);
                     float1 = float.Parse(return_text[3]);
                     parts[2].transform.eulerAngles = new Vector3(
